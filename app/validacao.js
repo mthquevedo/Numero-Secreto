b2dfcf867197330250ea5718e97d4b9658772425
Matheus Quevedo <matheusq10@gmail.com>
2023-10-21 16:49:38 -0300

    Mecânica de reload e dicas

--- app/validacao.js
+++ app/validacao.js
@@ -3,19 +3,32 @@ function verificaChute(chute) {
 
     if (chuteInvalido(numero)) {
         elementoChute.innerHTML += '<div class="warning">Sua resposta deve ser um número!</div>'
+        return
     }
 
     if (numeroMaiorOuMenor(numero)) {
         elementoChute.innerHTML += `
         <div class="warning">Fale um número entre ${menorValor} e ${maiorValor}!</div>
         `
+        return
     }
 
     if (numero === numeroSecreto) {
         elementoChute.innerHTML += `
         <p class="win">Acertou!</p>
+
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+         `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div class="warning">O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div class="warning">O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
+
 }
 
 function chuteInvalido(numero) {
@@ -25,3 +38,9 @@ function chuteInvalido(numero) {
 function numeroMaiorOuMenor(numero) {
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
